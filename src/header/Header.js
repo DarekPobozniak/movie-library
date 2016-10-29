7894c1aba80c31169474e0acb01a7bc9c8af59dd
Dariusz Pobożniak <dariusz.pobozniak@performgroup.com>
2016-10-30 00:30:29 +0200

    Linkt logo with main page

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 import './_styles.scss';
 
 const Header = () => (
   <header className="page-header">
     <div className="wrapper">
-      <h1 className="page-title">Movie Library</h1>
+      <h1 className="page-title"><Link to="/">Movie Library</Link></h1>
     </div>
   </header>
 );
