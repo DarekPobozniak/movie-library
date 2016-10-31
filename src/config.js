0f7dd9b104aceb939b6d669b96fb2f6b4593bdf2
Dariusz Pobożniak <dariusz.pobozniak@performgroup.com>
2016-10-31 13:45:32 +0100

    Extract apiVersion to separate const

--- src/config.js
+++ src/config.js
@@ -2,8 +2,11 @@
  * Config used to make requests to The Movie Database API
  * @type {Object}
  */
+
+const apiVersion = 3;
+
 export default {
-  baseUrl: 'https://api.themoviedb.org/3',
+  baseUrl: `https://api.themoviedb.org/${apiVersion}`,
   apiKey: '7865ca4c4fd2d00ff34dc331b1ca13b6',
   language: 'en-US',
 };
